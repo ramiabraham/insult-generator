5a745d2590d8136fdc8b85c9c44709ede2d4d388
robneu <rob@wpsitecare.com>
2014-09-12 11:01:15 -0500

    You're killing me Rami

--- plugin.php
+++ plugin.php
@@ -23,7 +23,7 @@ Author URI: http://ramiabraham.com
 */
 
 
-if ( ! function_exists('insult_generator_shortcode')) {
+if ( ! function_exists( 'insult_generator_shortcode' ) ) {
 
 	function insult_generator_shortcode( $atts, $content = null ) {
 
@@ -34,98 +34,99 @@ if ( ! function_exists('insult_generator_shortcode')) {
 
 		// Baroque by default yo
 
-		$ig_adjective_one = array( "artless",
-								   "bawdy",
-								   "beslubbering",
-								   "bootless",
-								   "churlish",
-								   "cockered",
-								   "clouted",
-								   "craven",
-								   "currish",
-								   "dankish",
-								   "dissembling",
-								   "droning",
-								   "errant",
-								   "fawning",
-								   "fobbing",
-								   "froward",
-								   "frothy",
-								   "gleeking",
-								   "goatish",
-								   "gorbellied",
-								   "impertinent",
-								   "infectious",
-								   "jarring",
-								   "loggerheaded",
-								   "lumpish",
-								   "mammering",
-								   "mangled",
-								   "mewling",
-								   "paunchy",
-								   "pribbling",
-								   "puking",
-								   "puny",
-								   "qualling",
-								   "rank",
-								   "reeky",
-								   "roguish",
-								   "ruttish",
-								   "saucy",
-								   "spleeny",
-								   "spongy",
-								   "surly",
-								   "tottering",
-								   "unmuzzled",
-								   "vain",
-								   "venomed",
-								   "villainous",
-								   "warped",
-								   "wayward",
-								   "weedy",
-								   "yeasty"
-								   );
+		$ig_adjective_one = array(
+			'artless',
+		    'bawdy',
+		    'beslubbering',
+		    'bootless',
+		    'churlish',
+		    'cockered',
+		    'clouted',
+		    'craven',
+		    'currish',
+		    'dankish',
+		    'dissembling',
+		    'droning',
+		    'errant',
+		    'fawning',
+		    'fobbing',
+		    'froward',
+		    'frothy',
+		    'gleeking',
+		    'goatish',
+		    'gorbellied',
+		    'impertinent',
+		    'infectious',
+		    'jarring',
+		    'loggerheaded',
+		    'lumpish',
+		    'mammering',
+		    'mangled',
+		    'mewling',
+		    'paunchy',
+		    'pribbling',
+		    'puking',
+		    'puny',
+		    'qualling',
+		    'rank',
+		    'reeky',
+		    'roguish',
+		    'ruttish',
+		    'saucy',
+		    'spleeny',
+		    'spongy',
+		    'surly',
+		    'tottering',
+		    'unmuzzled',
+		    'vain',
+		    'venomed',
+		    'villainous',
+		    'warped',
+		    'wayward',
+		    'weedy',
+		    'yeasty'
+	   );
 
-		$ig_adjective_two = array( "base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten" );
+		$ig_adjective_two = array( 'base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained', 'clapper-clawed', 'clay-brained', 'common-kissing', 'crook-pated', 'dismal-dreaming', 'dizzy-eyed', 'doghearted', 'dread-bolted', 'earth-vexing', 'elf-skinned', 'fat-kidneyed', 'fen-sucked', 'flap-mouthed', 'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged', 'guts-griping', 'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed', 'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered', 'motley-minded', 'onion-eyed', 'plume-plucked', 'pottle-deep', 'pox-marked', 'reeling-ripe', 'rough-hewn', 'rude-growing', 'rump-fed', 'shard-borne', 'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited', 'tickle-brained', 'toad-spotted', 'unchin-snouted', 'weather-bitten' );
 
-		$ig_noun = array( "apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail" );
+		$ig_noun = array( 'apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'joithead', 'lewdster', 'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate', 'strumpet', 'varlot', 'vassal', 'whey-face', 'wagtail' );
 
 		// Use baroque insults
 
 		if ( $type == 'baroque' ) {
 
-			$ig_adjective_one = array( "artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "fobbing", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "mewling", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "spongy", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty" );
+			$ig_adjective_one = array( 'artless', 'bawdy', 'beslubbering', 'bootless', 'churlish', 'cockered', 'clouted', 'craven', 'currish', 'dankish', 'dissembling', 'droning', 'errant', 'fawning', 'fobbing', 'froward', 'frothy', 'gleeking', 'goatish', 'gorbellied', 'impertinent', 'infectious', 'jarring', 'loggerheaded', 'lumpish', 'mammering', 'mangled', 'mewling', 'paunchy', 'pribbling', 'puking', 'puny', 'qualling', 'rank', 'reeky', 'roguish', 'ruttish', 'saucy', 'spleeny', 'spongy', 'surly', 'tottering', 'unmuzzled', 'vain', 'venomed', 'villainous', 'warped', 'wayward', 'weedy', 'yeasty' );
 
-			$ig_adjective_two = array( "base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten" );
+			$ig_adjective_two = array( 'base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained', 'clapper-clawed', 'clay-brained', 'common-kissing', 'crook-pated', 'dismal-dreaming', 'dizzy-eyed', 'doghearted', 'dread-bolted', 'earth-vexing', 'elf-skinned', 'fat-kidneyed', 'fen-sucked', 'flap-mouthed', 'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged', 'guts-griping', 'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed', 'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered', 'motley-minded', 'onion-eyed', 'plume-plucked', 'pottle-deep', 'pox-marked', 'reeling-ripe', 'rough-hewn', 'rude-growing', 'rump-fed', 'shard-borne', 'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited', 'tickle-brained', 'toad-spotted', 'unchin-snouted', 'weather-bitten' );
 
-			$ig_noun = array( "apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail", "lawyer" );
+			$ig_noun = array( 'apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'joithead', 'lewdster', 'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate', 'strumpet', 'varlot', 'vassal', 'whey-face', 'wagtail', 'lawyer' );
 		}
 
 		// Use boring modern insults. Needs work.
 
 		elseif ( $type == 'modern' ) {
 
-			$ig_adjective_one = array( "smarmy", "stinky", "farty", "cheese-toothed", "common" );
+			$ig_adjective_one = array( 'smarmy', 'stinky', 'farty', 'cheese-toothed', 'common' );
 
-			$ig_adjective_two = array( "contrarian", "smelly", "toilet-breathed" );
+			$ig_adjective_two = array( 'contrarian', 'smelly', 'toilet-breathed' );
 
-			$ig_noun = array( "ass-hat", "jerk", "ass-captain", "ass", "pig", "dick", "butt-burglar", "turd-burglar", "clack-dish", "fart-biscuit", "lawyer" );
+			$ig_noun = array( 'ass-hat', 'jerk', 'ass-captain', 'ass', 'pig', 'dick', 'butt-burglar', 'turd-burglar', 'clack-dish', 'fart-biscuit', 'lawyer' );
 
 		}
 
-		// Use insults from the movie "Hook"
+		// Use insults from the movie 'Hook'
 
 		elseif ( $type == 'pan' || $type == 'hook' || $type == 'peterpan' || $type == 'peter' ) {
 
-			$ig_adjective_one = array( "boil-dripping", "beefy", "fart-sniffing", "slug-slimed", "pinheaded", "two-toned", "zebra-headed", "slime-coated", "pimple-farmin'", "paramecium-brained" );
+			$ig_adjective_one = array( 'boil-dripping', 'beefy', 'fart-sniffing', 'slug-slimed', 'pinheaded', 'two-toned', 'zebra-headed', 'slime-coated', 'pimple-farmin\'', 'paramecium-brained' );
 
-			$ig_adjective_two = array( "scab-picked", "pimple-squeezing", "cheesy", "mung tongue", "camel-cake", "lying, crying, spying, prying" );
+			$ig_adjective_two = array( 'scab-picked', 'pimple-squeezing', 'cheesy', 'mung tongue', 'camel-cake', 'lying, crying, spying, prying' );
 
-			$ig_noun = array( "bubble-butt", "fart factory", "sack-of-rat-guts-in-cat-vomit", "finger bandage", "week old maggot burger with everything on it and flies on the side", "substitute chemistry teacher", "math tutor", "prison barber", "near-sighted gynecologist", "ultra-pig", "lewd, crude, rude, bag of pre-chewed food dude", "maggot burger", "lawyer" );
+			$ig_noun = array( 'bubble-butt', 'fart factory', 'sack-of-rat-guts-in-cat-vomit', 'finger bandage', 'week old maggot burger with everything on it and flies on the side', 'substitute chemistry teacher', 'math tutor', 'prison barber', 'near-sighted gynecologist', 'ultra-pig', 'lewd, crude, rude, bag of pre-chewed food dude', 'maggot burger', 'lawyer' );
 
 		}
 
-		// Prepend "fucking" to the insult
+		// Prepend 'fucking' to the insult
 
 		if ( $dirty == 'yes' ) {
 
@@ -140,6 +141,6 @@ if ( ! function_exists('insult_generator_shortcode')) {
 
 	}
 
-	add_shortcode("insult", "insult_generator_shortcode");
+	add_shortcode( 'insult', 'insult_generator_shortcode' );
 
 }
